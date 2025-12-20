7c448d999a46cdd069416e8d91dc76a6238183d6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-20 08:21:00 +0100

    feat: add contract constants

--- src/js/transactions.js
+++ src/js/transactions.js
@@ -7,3 +7,7 @@ import {
 import { network, userSession } from './wallet.js';
 
 console.log("Transactions module loaded");
+
+// Contract Constants
+export const CONTRACT_ADDRESS = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
+export const CONTRACT_NAME = 'DefiDungeons';
